77082b0dc7d64dd6e7a33ab0d7b571c4d23ca61b
Eric Chiu <e.chiu@snowflake.com>
2025-03-27 14:58:08 -0700

    [theming] Fine tune element spacing for `baseRadius` config (#10915)

--- frontend/lib/src/components/shared/Dropdown/Selectbox.tsx
+++ frontend/lib/src/components/shared/Dropdown/Selectbox.tsx
@@ -234,7 +234,7 @@ const Selectbox: React.FC<Props> = ({
             style: () => ({
               // Baseweb requires long-hand props, short-hand leads to weird bugs & warnings.
               paddingRight: theme.spacing.sm,
-              paddingLeft: theme.spacing.sm,
+              paddingLeft: theme.spacing.md,
               paddingBottom: theme.spacing.sm,
               paddingTop: theme.spacing.sm,
             }),
